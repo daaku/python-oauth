d74fd6986daa4586b35c1ed79eb5b7c86bce53bb
Naitik Shah <naitik.shah@yahoo.com>
2009-02-15 21:52:43 -0800

    added pip compatible setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oauth',
+    version='0.0.1',
+    description='OAuth Signature Generation/Validation',
+    author='Naitik Shah',
+    author_email='n@daaku.org',
+    url='https://code.daaku.org/python-oauth/',
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools', 'setuptools_git'],
+)
